7b8ccae2e7fef81894d6deaa5021eb134b874e20
Manh Hung <nmhung1998@gmail.com>
2021-06-07 14:58:45 +0700

    Add source web ecomerce

---
+++ public/js/frontend/account.js
@@ -0,0 +1,26 @@
+function updatePass() {
+	axios.post('/password/update', $('#form_change_pass').serialize())
+	.then(function(response){
+		Swal.fire({
+			icon: 'success',
+			title: 'Thông báo',
+			text: response.data.message,
+		}).then((result) => {
+			window.location.href = '/';
+		});
+	}).catch(function(error){
+		var data = error.response.data;
+		if (data.status == 'validator_fail') {
+			var messages = data.messages;
+			Object.keys(messages).forEach(key => {
+				$('#'+key+'_error').text(messages[key][0]);
+			});
+		} else{
+			$('#message_error').text(data.messages);
+		} 
+	})
+}
+function removeMessage(obj){
+	var input_name = obj.attr('name');
+	$('#'+input_name+'_error').text('');
+}
